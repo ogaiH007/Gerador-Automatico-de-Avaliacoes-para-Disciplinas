bee045967407efb2a6f8cf4934a0b31279ae7f9d
Hiago Luiz <ogaih007@gmail.com>
2023-09-04 18:12:58 -0300

    Create Aberta.java

---
+++ src/main/Aberta.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Aberta extends Questao{
+	private String resposta;
+	private String respostaAproximada;
+	
+	public String getResposta() {
+		return resposta;
+	}
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
+	public String getRespostaAproximada() {
+		return respostaAproximada;
+	}
+	public void setRespostaAproximada(String respostaAproximada) {
+		this.respostaAproximada = respostaAproximada;
+	}
+}
