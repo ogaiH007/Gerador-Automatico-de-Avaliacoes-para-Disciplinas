5530696b33f3ef182b7b3c7b1267fb7471ac7569
Hiago Luiz <ogaih007@gmail.com>
2023-09-04 18:12:52 -0300

    Create VerdadeiroFalso.java

---
+++ src/main/VerdadeiroFalso.java
@@ -0,0 +1,19 @@
+package main;
+
+public class VerdadeiroFalso extends Questao{
+	private String[] opcoes;
+	private char[] opcoesResposta;
+	
+	public String[] getOpcoes() {
+		return opcoes;
+	}
+	public void setOpcoes(String[] opcoes) {
+		this.opcoes = opcoes;
+	}
+	public char[] getOpcoesResposta() {
+		return opcoesResposta;
+	}
+	public void setOpcoesResposta(char[] opcoesResposta) {
+		this.opcoesResposta = opcoesResposta;
+	}
+}
