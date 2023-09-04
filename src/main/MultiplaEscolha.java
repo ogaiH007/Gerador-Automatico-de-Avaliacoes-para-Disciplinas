160ee39462059aa45e3fb91478bd98fcdc3d898d
Hiago Luiz <ogaih007@gmail.com>
2023-09-04 18:02:39 -0300

    Create MultiplaEscolha.java

---
+++ src/main/MultiplaEscolha.java
@@ -0,0 +1,19 @@
+package main;
+
+public class MultiplaEscolha extends Questao{
+	private String[] alternativas;
+	private int alternativaCorreta;
+	
+	public String[] getAlternativas() {
+		return alternativas;
+	}
+	public void setAlternativas(String[] alternativas) {
+		this.alternativas = alternativas;
+	}
+	public int getAlternativaCorreta() {
+		return alternativaCorreta;
+	}
+	public void setAlternativaCorreta(int alternativaCorreta) {
+		this.alternativaCorreta = alternativaCorreta;
+	}
+}
