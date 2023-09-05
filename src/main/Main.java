abd9c7f188129b6cc8a4726f3c1c1f6aa46d690e
Hiago Luiz <ogaih007@gmail.com>
2023-09-05 00:00:36 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,8 +3,10 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//int numQuestoes = 1;
+		MultiplaEscolha questao1 = new MultiplaEscolha();
+		
+		questao1.setEnunciado("Qual é a capital de pernambuco");
+		//questao1.setAlternativas({"Recife", "Salvador", "Curitiba", "Manaus"});
 	}
-
 }
