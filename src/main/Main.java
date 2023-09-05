d454e9c4c49f2de45c6d735ec6b1c35427a4fdc2
Hiago Luiz <ogaih007@gmail.com>
2023-09-05 18:30:52 -0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,28 @@ package main;
 
 public class Main {
 
-	public static void main(String[] args) {
-		//int numQuestoes = 1;
-		MultiplaEscolha questao1 = new MultiplaEscolha();
-		
-		questao1.setEnunciado("Qual é a capital de pernambuco");
-		//questao1.setAlternativas({"Recife", "Salvador", "Curitiba", "Manaus"});
-	}
+    public static void main(String[] args) {
+        int i;
+    	int numQuestoesMultiplaEscolha = 1;
+        int numQuestoesVerdadeiroFalso = 1;
+        int numQuestoesAberta = 1;
+        
+        int numQuestoesTotal = numQuestoesAberta + numQuestoesMultiplaEscolha + numQuestoesVerdadeiroFalso;
+        MultiplaEscolha[] questao = new MultiplaEscolha[numQuestoesMultiplaEscolha];
+
+        for (i = 0; i < numQuestoesMultiplaEscolha; i++) {
+            questao[i] = new MultiplaEscolha();
+            questao[i].setEnunciado("Qual é a capital de Pernambuco?");
+            questao[i].setAlternativas(new String[]{"Recife", "Olinda", "Caruaru"});
+        }
+        
+        questao[0].addAlternativa("Salvador", 3);
+        questao[0].addAlternativa("Manaus", 4);
+        
+        System.out.println(questao[0].getEnunciado());
+        String[] alternativas = questao[0].getAlternativas();
+        for (i = 0; i < alternativas.length; i++) {
+            System.out.println(alternativas[i]);
+        }
+    }
 }
