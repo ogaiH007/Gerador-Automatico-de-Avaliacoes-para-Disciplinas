d6aab3f45804f07c636083fd284c98173b6e46ee
Hiago Luiz <ogaih007@gmail.com>
2023-09-05 18:36:58 -0300

    Update VerdadeiroFalso.java

--- src/main/VerdadeiroFalso.java
+++ src/main/VerdadeiroFalso.java
@@ -16,4 +16,13 @@ public class VerdadeiroFalso extends Questao{
 	public void setOpcoesResposta(char[] opcoesResposta) {
 		this.opcoesResposta = opcoesResposta;
 	}
+	public void addOpcao(String opcao, int index) {
+		if (index >= opcoes.length) {
+            // Aumentar o tamanho do array ou usar ArrayList em vez de array regular
+            String[] temp = new String[index + 1];
+            System.arraycopy(opcoes, 0, temp, 0, opcoes.length);
+            opcoes = temp;
+        }
+        opcoes[index] = opcao;
+    }
 }
