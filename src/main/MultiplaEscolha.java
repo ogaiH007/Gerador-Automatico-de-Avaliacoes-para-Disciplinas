fc0cc847c61ce1fc83717ca43f30c97894f542f4
Hiago Luiz <ogaih007@gmail.com>
2023-09-05 18:30:47 -0300

    Update MultiplaEscolha.java

--- src/main/MultiplaEscolha.java
+++ src/main/MultiplaEscolha.java
@@ -8,13 +8,22 @@ public class MultiplaEscolha extends Questao{
 	public String[] getAlternativas() {
 		return alternativas;
 	}
-	public void setAlternativas(String alternativa, int posicaoAlternativa) {
-		this.alternativas[posicaoAlternativa] = alternativa;
-	}
+	public void setAlternativas(String[] alternativas) {
+        this.alternativas = alternativas;
+    }
 	public int getAlternativaCorreta() {
 		return alternativaCorreta;
 	}
 	public void setAlternativaCorreta(int alternativaCorreta) {
 		this.alternativaCorreta = alternativaCorreta;
 	}
+	public void addAlternativa(String alternativa, int index) {
+		if (index >= alternativas.length) {
+            // Aumentar o tamanho do array ou usar ArrayList em vez de array regular
+            String[] temp = new String[index + 1];
+            System.arraycopy(alternativas, 0, temp, 0, alternativas.length);
+            alternativas = temp;
+        }
+        alternativas[index] = alternativa;
+    }
 }
